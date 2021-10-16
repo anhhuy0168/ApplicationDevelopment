0da0c838b461ea117335230c37a4759e25770264
huy nguyen anh <anhhuy123zxc123@gmail.com>
2021-10-16 22:41:46 +0700

    Create Staff Table

--- ApplicationDevelopment/Models/ApplicationDbContext.cs
+++ ApplicationDevelopment/Models/ApplicationDbContext.cs
@@ -11,6 +11,7 @@ namespace ApplicationDevelopment.Models
         }
         public DbSet<CourseCategory> CourseCategories { get; set; }
         public DbSet<Course> Courses { get; set; }
+        public DbSet<Staff> Staffs { get; set; }
         public static ApplicationDbContext Create()
         {
             return new ApplicationDbContext();
